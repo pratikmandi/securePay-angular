ca4253a3c05299fe76ebc5fede0b0dc7fd8298ea
Pratik Mandi <pratikmandi0984@gmail.com>
2026-05-09 16:37:03 +0530

    Update index.js: added allowed origin, deployed site on render.com

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,15 @@ env.config()
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://securepay-angular-1.onrender.com'
+];
+
 app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:4200']
-}))
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(cookieParser())
 app.use(express.json())
 app.use("/auth", routes)
